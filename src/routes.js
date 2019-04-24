e6d9e096d2c23dffc598fc8f5689742997025048
Benjamin Hathoot <42647000+Bhathoot@users.noreply.github.com>
2019-04-24 14:16:03 -0500

    Working webpage

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
-import Login from "./containers/Login";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from './containers/Home';
+import Login from './containers/Login';
+import AppliedRoute from "./components/AppliedRoute";
 
-export const ROUTES = [
-    { path:"/login",  component:Login },
-];
+import NotFound from "./containers/NotFound";
 
-export default ROUTES;
+export default ({ childProps }) =>
+  <Switch>
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    { /* Finally, catch all unmatched routes */ }
+    <Route component={NotFound} />
+  </Switch>;
+
